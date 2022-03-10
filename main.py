d8c62bea6af0346192fbcbf0762595d2e3992860
Guilherme Benfica <99348004+Guilherme-Benfica@users.noreply.github.com>
2022-03-10 19:19:09 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+soma: int
+x: 0
+soma = 0
+
+x = int(input("Digite o primeiro numero: "))
+
+while x !=0:
+    soma = soma + x
+    x = int(input("Digite outro numero: "))
+
+print("SOMA =", soma)
+
